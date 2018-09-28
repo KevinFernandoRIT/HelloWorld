241ad052b9711e30c8de456bbdf9ebc4296a0284
Kevin Fernando <kkf4497@rit.edu>
2018-09-27 20:56:05 -0400

    Added files

---
+++ app/src/main/java/kkf4497/helloworld/Kevins_Activity.java
@@ -0,0 +1,13 @@
+package kkf4497.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Kevins_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kevins_);
+    }
+}
