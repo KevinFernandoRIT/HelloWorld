55a61949882b4d3d685c38247505938bbc77b4c7
Nick Capadano <nc8005@g.rit.edu>
2018-10-04 14:42:32 -0400

    Created second Activity: "nc80052"

---
+++ app/src/main/java/kkf4497/helloworld/nc80052.java
@@ -0,0 +1,14 @@
+package kkf4497.helloworld;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class nc80052 extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nc80052);
+    }
+
+}
