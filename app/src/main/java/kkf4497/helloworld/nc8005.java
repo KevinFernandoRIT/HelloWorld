11feba323eb63aece57814f3c6d134a5e2f9bbf5
Nick Capadano <nc8005@g.rit.edu>
2018-10-04 15:44:39 -0400

    Completed Tutorial

--- app/src/main/java/kkf4497/helloworld/nc8005.java
+++ app/src/main/java/kkf4497/helloworld/nc8005.java
@@ -2,12 +2,34 @@ package kkf4497.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Context;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 
 public class nc8005 extends AppCompatActivity {
-
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nc8005);
+
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+                openNc80052();
+            }
+
+
+        });
+    }
+
+    public void openNc80052() {
+
+        Intent intent = new Intent(this, nc80052.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/kkf4497/helloworld/nc80052.java
+++ app/src/main/java/kkf4497/helloworld/nc80052.java
@@ -1,14 +1,33 @@
 package kkf4497.helloworld;
 
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 
-public class nc80052 extends Activity {
-
+public class nc80052 extends AppCompatActivity {
+    private Button button2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nc80052);
+
+        button2 = (Button) findViewById(R.id.button2);
+
+        button2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+                openNc8005();
+            }
+        });
     }
 
+    public void openNc8005() {
+
+        Intent intent = new Intent(this, nc8005.class);
+        startActivity(intent);
+    }
 }
