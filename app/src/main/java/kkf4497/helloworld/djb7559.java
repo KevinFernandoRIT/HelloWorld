b669d0868955ffcf7e21a17e5f662530c3f8051b
djb7559 <djb7559@g.rit.edu>
2018-10-04 15:57:21 -0400

    working on tutorial

---
+++ app/src/main/java/kkf4497/helloworld/djb7559.java
@@ -0,0 +1,15 @@
+package kkf4497.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class djb7559 extends AppCompatActivity {
+    public Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_djb7559);
+    }
+}
---
+++ app/src/main/java/kkf4497/helloworld/djb7559_2.java
@@ -0,0 +1,13 @@
+package kkf4497.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class djb7559_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_djb7559_2);
+    }
+}
